77fd60e3e5cfdf6e3dfe071393158fc04e83b1be
Dmitrijjj <dmytropryjmak.dp@gmail.com>
2022-10-14 16:42:41 +0300

    created adapter

---
+++ app/src/main/java/com/dimidroid/notekeeper/NoteAdapter.java
@@ -0,0 +1,56 @@
+package com.dimidroid.notekeeper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder>{
+
+    List<Note> noteList = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.note_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+
+        Note currentNote = noteList.get(position);
+
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.textViewDate.setText(currentNote.getDate());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteList.size();
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewTitle, textViewDescription, textViewDate;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDesc);
+            textViewDate = itemView.findViewById(R.id.textViewDate);
+
+        }
+    }
+
+}
