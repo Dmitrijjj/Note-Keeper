7843fd5d932fc0a4bfe2f30f68d44827ea5673ed
Dmitrijjj <dmytropryjmak.dp@gmail.com>
2022-10-14 16:28:43 +0300

    made all ui

--- app/src/main/java/com/dimidroid/notekeeper/MainActivity.java
+++ app/src/main/java/com/dimidroid/notekeeper/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dimidroid.notekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Insert;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, EditNoteActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
