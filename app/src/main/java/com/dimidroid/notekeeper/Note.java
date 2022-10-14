b736a7d9b9c577570191a3c6a1558fc9557a794e
Dmitrijjj <dmytropryjmak.dp@gmail.com>
2022-10-14 13:36:26 +0300

    added dependencies & model & dao

---
+++ app/src/main/java/com/dimidroid/notekeeper/Note.java
@@ -0,0 +1,43 @@
+package com.dimidroid.notekeeper;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    public Note(String title, String description, String date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private final String title;
+
+    private final String description;
+
+    private final String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
