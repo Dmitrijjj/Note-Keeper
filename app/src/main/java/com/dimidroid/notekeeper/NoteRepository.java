72f74c295b19675e646268cf0d91534c105cabc5
Dmitrijjj <dmytropryjmak.dp@gmail.com>
2022-10-14 13:54:44 +0300

    made repository

--- app/src/main/java/com/dimidroid/notekeeper/NoteRepository.java
+++ app/src/main/java/com/dimidroid/notekeeper/NoteRepository.java
@@ -1,2 +1,66 @@
-package com.dimidroid.notekeeper;public class NoteRepository {
+package com.dimidroid.notekeeper;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NoteRepository {
+
+    private NoteDao dao;
+    private NoteDatabase noteDatabase;
+
+    LiveData<List<Note>> noteList;
+
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    public NoteRepository(Application application){
+
+        noteDatabase = NoteDatabase.getInstance(application);
+        NoteDao dao = noteDatabase.dao();
+        noteList = dao.getAllNotes();
+
+    }
+
+    public void add(Note note){
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.add(note);
+            }
+        });
+
+    }
+
+    public void edit(Note note){
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.edit(note);
+            }
+        });
+
+    }
+
+    public void delete(Note note){
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.delete(note);
+            }
+        });
+
+    }
+
+    LiveData<List<Note>> getAllNotes(){
+
+        return noteList;
+
+    }
 }
