0c4309db7c3359ed994abefcf290f914946288b1
Dmitrijjj <dmytropryjmak.dp@gmail.com>
2022-10-14 13:44:10 +0300

    made db

---
+++ app/src/main/java/com/dimidroid/notekeeper/NoteDatabase.java
@@ -0,0 +1,29 @@
+package com.dimidroid.notekeeper;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    public abstract NoteDao dao();
+
+    public static NoteDatabase instance;
+
+    public static synchronized NoteDatabase getInstance(Context context){
+
+        if (instance == null){
+
+            instance = Room.databaseBuilder(
+                    context, NoteDatabase.class, "Note_DB")
+                    .fallbackToDestructiveMigration().build();
+        }
+
+        return instance;
+
+    }
+
+}
